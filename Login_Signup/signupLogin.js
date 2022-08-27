4eec49df800351b8d6e027c4223682187a8ad965
juhidiwase99 <jdiwase2017@gmail.com>
2022-08-28 00:49:30 +0530

    final check

--- Login_Signup/signupLogin.js
+++ Login_Signup/signupLogin.js
@@ -1,24 +1,35 @@
 
-// class AccData{
-//     constructor(e,pass){
-//         this.email = e;
-//         this.password = pass;
-//     }
-// }  
-function submit() {
-    event.preventDefault();
-    let Data = JSON.parse(localStorage.getItem("AccountData")) || [];
-    let Email = document.getElementById('email').value;
-    let Password = document.getElementById('Password').value;
-    if(Email==Data.Email && Password==Data.password){
-        alert("Login succesfully")
-    }
-    else if(Email==Data.email && Password!=Data.password){
-        alert("Password dose not match")
-    }
-    else if(Email!=Data.email && Password==Data.password){
-        alert("User dose not found")
-    }
-    console.log(Data.email)
+function submit(){
+    let email=document.querySelector("#email").value;
+    let password=document.querySelector("#Password").value;
+
+    let a=validate(email, password);
+    
 }
 
+function validate(email, password){
+    let users=JSON.parse(localStorage.getItem("users"))||[];
+    let count=0;
+    for(let ele of users){
+        if(ele.Email==email){
+            count++;
+            if(ele.Password==password){
+                ele.Login=true;
+                localStorage.setItem("users",JSON.stringify(users));
+                let userStatus=[];
+                userStatus.push(ele);
+                localStorage.setItem("status",JSON.stringify(userStatus));
+                alert("Login Successful!");
+                return true;
+            }
+            else{
+                alert("Wrong credentials");
+                return false;
+            }
+        }
+    }
+    if(count==0){
+        alert("User doesn't exist, Sign Up");
+        window.location.href="signup.html";
+    }
+}
