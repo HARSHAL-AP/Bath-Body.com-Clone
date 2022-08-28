ee97fcfa042ce41b60c2b02f36391bff5e52ddcf
HARSHAL-AP <apsundeharshal129@gmail.com>
2022-08-28 15:25:54 +0530

    chenging login function

--- Login_Signup/createAcc.js
+++ Login_Signup/createAcc.js
@@ -1,32 +1,39 @@
 
-function main(){
-    let name=document.querySelector("#CAname").value;
-    let email=document.querySelector("#CAemail").value;
-    let password=document.querySelector("#CApass").value;
 
-    let users=JSON.parse(localStorage.getItem("users"))||[];
-    let a=users.filter(function(ele){
-        return ele.Email==email;
-    })
-    if(a.length!=0) alert("User Email Already exist")
-    else{
-        saveUserInLS(name, email, password);
+
+function  creat_acount(event){
+    event.preventDefault()
+    var arr=JSON.parse(localStorage.getItem("User"))||[]
+    let bag=true
+    for(let i=0;i<arr.length;i++){
+        if(arr[i].email===document.getElementById("CAemail").value){
+            bag=false;
+            alert("User Alrady exist Please Login ..!")
+           break;
+        }
     }
-}
-function saveUserInLS(a, b ,c){
-    let user1=new User(a, b, c);
-    let users=JSON.parse(localStorage.getItem("users"))||[];
-    users.push(user1);
-    localStorage.setItem("users",JSON.stringify(users));
-}
-
-class User{
-    constructor(name, email, password){
-        this.Name=name;
-        this.Email=email;
-        this.Password=password;
-        this.Login=false;
+    
+    if(bag===true){
+        let user={
+            "email":document.getElementById("CAemail").value,
+            "password":document.getElementById("CApass").value,
+        }
+        arr.push(user)
+        localStorage.setItem("User",JSON.stringify(arr))
+        alert("Sign up Successful...")
+       gotologinpage()
     }
-}
+    
+    
+    
+    
+    
+    
+    }
+
+    function  gotologinpage(){
+
+window.location.href="/Login_Signup/signupLogin.html"
 
 
+    }
--- Login_Signup/signupLogin.js
+++ Login_Signup/signupLogin.js
@@ -1,60 +1,22 @@
-
-<<<<<<< HEAD
-// class AccData{
-//     constructor(e,pass){
-//         this.email = e;
-//         this.password = pass;
-//     }
-// }  
-function submit() {
-    event.preventDefault();
-    let Data = JSON.parse(localStorage.getItem("AccountData")) ||null;
-    let Email = document.getElementById('email').value;
-    let Password = document.getElementById('Password').value;
-    if(Email==Data.email && Password==Data.password){
-        alert("Login succesfully")
-        window.location.href="/index.html"
-    }
-    else if(Email==Data.email && Password!=Data.password){
-        alert("Password dose not match")
-    }
-    else if(Email!=Data.email && Password==Data.password){
-        alert("User dose not found")
-    }
-//console.log(Data)
-=======
-function submit(){
-    let email=document.querySelector("#email").value;
-    let password=document.querySelector("#Password").value;
-
-    let a=validate(email, password);
+function mainlogin(){
     
->>>>>>> 4688d2284f6677a0b76b6c9ea4b0f112e21cd5f2
-}
+    let arr=JSON.parse(localStorage.getItem("User"))||[]
+    let bag=false
+    for(let i=0;i<arr.length;i++){
+       if(arr[i].email===document.getElementById("email").value && arr[i].password===document.getElementById("Password").value){
+       bag=true;
+       break;
+       }
+       
+    }
+   
 
-function validate(email, password){
-    let users=JSON.parse(localStorage.getItem("users"))||[];
-    let count=0;
-    for(let ele of users){
-        if(ele.Email==email){
-            count++;
-            if(ele.Password==password){
-                ele.Login=true;
-                localStorage.setItem("users",JSON.stringify(users));
-                let userStatus=[];
-                userStatus.push(ele);
-                localStorage.setItem("status",JSON.stringify(userStatus));
-                alert("Login Successful!");
-                return true;
-            }
-            else{
-                alert("Wrong credentials");
-                return false;
-            }
-        }
-    }
-    if(count==0){
-        alert("User doesn't exist, Sign Up");
-        window.location.href="signup.html";
-    }
+   if(bag===true){
+    alert("Login Sucssesfull...!")
+    window.location.href="/index.html"
+   }
+   else{
+    alert("Incorect Password")
+   }
+  
 }
